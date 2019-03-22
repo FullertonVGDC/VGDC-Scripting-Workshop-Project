9f9e3590035a6c743958c316a6dd083e0c8b4839
Michael Perez <mperez980089@yahoo.com>
2019-03-22 01:34:34 -0700

    Singleton example

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    public static GameMaster instance;
     public int targetFrameRate = 60;
+
+    [HideInInspector]
+    public float score;
+
+    [HideInInspector]
+    public float screenTopEdge;
+    [HideInInspector]
+    public float screenBottomEdge;
+    [HideInInspector]
+    public float screenLeftEdge;
+    [HideInInspector]
+    public float screenRightEdge;
+
+
 
     void Start()
     {
+        if (instance != null && instance != this)
+            Destroy(gameObject);
+        else
+            instance = this;
+        DontDestroyOnLoad(this);
+
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = targetFrameRate;
+
+        UpdateScreenEdges();
     }
 
     void Update()
     {
-        
+        UpdateScreenEdges();
+    }
+
+
+
+    void UpdateScreenEdges()
+    {
+        screenTopEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, -(Camera.main.transform.position.z))).y;
+        screenBottomEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -(Camera.main.transform.position.z))).y;
+        screenLeftEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -(Camera.main.transform.position.z))).x;
+        screenRightEdge = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, -(Camera.main.transform.position.z))).x;
     }
 }
---
+++ Assets/Scripts/SingletonPlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonPlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    void Update()
+    {
+        Vector2 moveAmount = new Vector2();
+        moveAmount.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveAmount.y = Input.GetAxisRaw("Vertical") * speed;
+        transform.Translate(moveAmount * Time.deltaTime);
+
+        //Prevent going off right edge
+        if (transform.position.x > GameMaster.instance.screenRightEdge)
+            transform.position = new Vector2(GameMaster.instance.screenRightEdge, transform.position.y);
+
+        //Prevent going off left edge
+        if (transform.position.x < GameMaster.instance.screenLeftEdge)
+            transform.position = new Vector2(GameMaster.instance.screenLeftEdge, transform.position.y);
+
+        //Prevent going off top edge
+        if (transform.position.y > GameMaster.instance.screenTopEdge)
+            transform.position = new Vector2(transform.position.x, GameMaster.instance.screenTopEdge);
+
+        //Prevent going off bottom edge
+        if (transform.position.y < GameMaster.instance.screenBottomEdge)
+            transform.position = new Vector2(transform.position.x, GameMaster.instance.screenBottomEdge);
+    }
+}
