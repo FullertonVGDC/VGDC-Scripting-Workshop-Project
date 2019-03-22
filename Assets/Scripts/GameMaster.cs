9ba97be7c1e01fecb1f733200e6404234570fa28
Michael Perez <mperez980089@yahoo.com>
2019-03-21 23:19:33 -0700

    Added delta time example

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public int targetFrameRate = 60;
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TranslateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateObject : MonoBehaviour
+{
+    public bool usesDeltaTime;
+    public float speed;
+
+    void Update()
+    {
+        Vector2 moveAmount = new Vector2();
+        moveAmount.x = (usesDeltaTime) ? (speed * Time.deltaTime) : speed;
+
+        transform.Translate(moveAmount);
+
+        if (transform.position.x > 10) transform.position = new Vector2(-10, transform.position.y);
+    }
+}
