79189039a2b431cc6005a247a9e2ed4ecb5a84ae
Michael Perez <mperez980089@yahoo.com>
2019-03-22 00:40:10 -0700

    Added enum example

---
+++ Assets/Scripts/EnumPlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumPlayerController : MonoBehaviour
+{
+    public enum State { Attacking, Defending, Fleeing };
+    State state;
+
+    SpriteRenderer sr;
+
+
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        ChangeState(State.Attacking);
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Attacking:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    ChangeState(State.Defending);
+                }
+                break;
+
+            case State.Defending:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    ChangeState(State.Fleeing);
+                }
+                break;
+
+            case State.Fleeing:
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    ChangeState(State.Attacking);
+                }
+                break;
+        }
+    }
+
+
+
+    void ChangeState(State newState)
+    {
+        switch (newState)
+        {
+            case State.Attacking:
+                state = State.Attacking;
+                sr.color = Color.red;
+                break;
+
+            case State.Defending:
+                state = State.Defending;
+                sr.color = Color.blue;
+                break;
+
+            case State.Fleeing:
+                state = State.Fleeing;
+                sr.color = Color.green;
+                break;
+        }
+    }
+}
