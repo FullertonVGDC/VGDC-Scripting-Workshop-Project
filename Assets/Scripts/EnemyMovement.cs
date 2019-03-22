aa3d6fe8f2fad4f5051e3258e197e8b96be05def
Michael Perez <mperez980089@yahoo.com>
2019-03-22 00:01:35 -0700

    Finished instantiate scene

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        if (transform.position.x > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnTimerLength;
+    float spawnTimer;
+
+    public GameObject enemyPrefab;
+
+
+
+    void Start()
+    {
+        spawnTimer = spawnTimerLength;
+    }
+
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            spawnTimer = spawnTimerLength;
+            Vector2 enemySpawnPosition = new Vector2(-10, Random.Range(-3.5f, 3.5f));
+            Instantiate(enemyPrefab, enemySpawnPosition, Quaternion.identity);
+        }
+    }
+}
